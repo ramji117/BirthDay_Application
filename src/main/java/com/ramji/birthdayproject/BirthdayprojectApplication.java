ad0d9a1a5a33152cf10fad8dc40e2bde937d1730
ramji <ramjirao11code@gmail.com>
2024-05-30 15:21:04 +0530

    initial commit

---
+++ src/main/java/com/ramji/birthdayproject/BirthdayprojectApplication.java
@@ -0,0 +1,13 @@
+package com.ramji.birthdayproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdayprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthdayprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramji/birthdayproject/BirthdayprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramji.birthdayproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdayprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
