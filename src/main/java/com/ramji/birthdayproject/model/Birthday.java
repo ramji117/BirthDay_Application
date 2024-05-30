1bfdb021e1d406ed99599e7a2049cc88147d0898
ramji <ramjirao11code@gmail.com>
2024-05-30 16:32:06 +0530

    bdaymodelcommit

--- src/main/java/com/ramji/birthdayproject/model/Birthday.java
+++ src/main/java/com/ramji/birthdayproject/model/Birthday.java
@@ -2,12 +2,35 @@ package com.ramji.birthdayproject.model;
 
 import java.util.Date;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="birthday_table")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Birthday {
+	
+	@Id
+	@GeneratedValue(generator = "uuid2")
+	@GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+	@Column(name = "id", columnDefinition = "VARCHAR(255)")
+	private String id;
 
 	private String name;
 	
-	
-	private String age;
-	
 	private Date dob;
+	
+	private Character gender;
 }
